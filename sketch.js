78091452b375b58a8a723c309e09e9e56aa8c417
sriharshini023 <ssudhasureshkumar@gmail.com>
2021-01-01 13:01:29 +0530

    final stage of angry bird

--- sketch.js
+++ sketch.js
@@ -81,9 +81,9 @@ function draw(){
 }
 
 function mouseDragged(){
-    //if (gameState!=="launched"){
+   if (gameState!=="launched"){
         Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-    //}
+    }
 }
 
 
@@ -94,18 +94,22 @@ function mouseReleased(){
 
 function keyPressed(){
     if(keyCode === 32){
+        Matter.Body.setPosition(bird.body, {x:200 , y: 50});
        slingshot.attach(bird.body);
+       bird.trajectory=[]
+       gameState="onSling"
+       
     }
 }
 
 async function getBackgroundImg(){
-    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var response = await fetch("http://worldclockapi.com/api/json/est/now");
     var responseJSON = await response.json();
 
-    var datetime = responseJSON.datetime;
+    var datetime= responseJSON.currentDateTime;
     var hour = datetime.slice(11,13);
     
-    if(hour>=0600 && hour<=1900){
+    if(hour>=06 && hour<=19){
         bg = "sprites/bg1.png";
     }
     else{
